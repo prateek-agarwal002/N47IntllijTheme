77c0c627540ab15f3e1d9bc27d3ecae60e83f3eb
GitHub Action <action@github.com>
2022-09-07 14:16:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/prateekagarwal002/n47intllijtheme/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/prateekagarwal002/n47intllijtheme/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.prateekagarwal002.n47intllijtheme.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.prateekagarwal002.n47intllijtheme.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/test/kotlin/com/github/prateekagarwal002/n47intllijtheme/MyPluginTest.kt
+++ src/test/kotlin/com/github/prateekagarwal002/n47intllijtheme/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.prateekagarwal002.n47intllijtheme
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
